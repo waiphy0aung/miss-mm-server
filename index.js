40022ab1a1c21d63b758a79171fbcd9158042d67
waiphyoaung <waiphyoaung17903@gmail.com>
2023-08-16 08:27:58 +0630

    update

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 dotenv.config()
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '30mb'}))
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
 app.use(morgan("common"))
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.json({ extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use("/assets", express.static(path.join(__dirname, "public/assets")))
 app.use(cors())
